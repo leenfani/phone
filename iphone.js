cf606ffb7af6a148fb7c76b44a359d667dea19d1
leenfani <leenbeg26.com>
2023-02-06 20:12:42 +0200

    first commit

---
+++ iphone.js
@@ -0,0 +1,8 @@
+let img0 = document.querySelector('.l');
+let container = document.querySelector('.container');
+function phone (phonesrc){
+    img0.src = phonesrc;
+}
+function color(bc){
+    container.style.background = bc;  
+}
